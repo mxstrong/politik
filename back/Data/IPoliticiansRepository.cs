c40c9e905500b6edfee9589100ae4736c32bde11
mxstrong <mantas.ptakauskas@gmail.com>
2021-10-14 15:07:02 +0300

    Add Politicians

---
+++ back/Data/IPoliticiansRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Politics.Data
+{
+  public interface IPoliticiansRepository
+  {
+    Task<PoliticianDto> AddPolitician(PoliticianDto politicianDto);
+    Task<List<PoliticianDto>> GetAllPoliticians();
+  }
+}
---
+++ back/Mapping/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Politics.Data;
+using Politics.Model;
+
+namespace Politics.Mapping
+{
+  public class MappingProfile : Profile
+  {
+    public MappingProfile()
+    {
+      CreateMap<Politician, PoliticianDto>()
+        .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.PoliticianId))
+        .ForMember(dest => dest.Party, opt => opt.MapFrom(src => src.Party.Name));
+      CreateMap<PoliticianDto, Politician>();
+    }
+  }
+}

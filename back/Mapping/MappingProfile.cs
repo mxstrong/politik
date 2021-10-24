988e7ff76b0272d58ca1bb6f7b3fcc89cbbf0975
mxstrong <mantas.ptakauskas@gmail.com>
2021-10-24 10:44:07 +0300

    change created and updated properties

--- back/Mapping/MappingProfile.cs
+++ back/Mapping/MappingProfile.cs
@@ -9,6 +9,7 @@ namespace Politics.Mapping
     public MappingProfile()
     {
       CreateMap<Politician, PoliticianOutDto>()
+        //.IncludeMembers(src => src.Party)
         .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.PoliticianId))
         .ForMember(dest => dest.Party, opt => opt.MapFrom(src => src.Party.ShortName))
         .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.FirstName + ' ' + src.LastName));
@@ -16,6 +17,15 @@ namespace Politics.Mapping
       CreateMap<Party, PartyOutDto>()
         .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.PartyId));
       CreateMap<PartyDto, Party>();
+      //CreateMap<TagDto, Tag>();
+      //CreateMap<Tag, TagDto>();
+      //CreateMap<StatementDto, Statement>();
+      //CreateMap<Tag, string>().ForMember(dest => dest, m => m.MapFrom(src => src.Name));
+      //CreateMap<Statement, StatementOutDto>()
+      //  .IncludeMembers(src => src.CreatedBy, src => src.Politician, src => src.Tags)
+      //  .ForMember(dest => dest.CreatedBy, opt => opt.MapFrom(src => src.CreatedBy.DisplayName))
+      //  .ForMember(dest => dest.Politician, opt => opt.MapFrom(src => src.Politician.FirstName + ' ' + src.Politician.LastName))
+      //  .ForMember(dest => dest.Tags, opt => opt.MapFrom(src => src.Tags));
     }
   }
 }

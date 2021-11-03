918ff82ae8f9d152fed06439aac35fbeca738a90
mxstrong <mantas.ptakauskas@gmail.com>
2021-11-03 11:22:13 +0200

    improve validation

--- back/Controllers/StatementsController.cs
+++ back/Controllers/StatementsController.cs
@@ -13,27 +13,46 @@ namespace Politics.Controllers
   [ApiController]
   public class StatementsController : ControllerBase
   {
-    private readonly IStatementsRepository _repo;
+    private readonly IStatementsRepository _statementsRepo;
+    private readonly IPoliticiansRepository _politiciansRepo;
 
-    public StatementsController(IStatementsRepository repo)
+    public StatementsController(IStatementsRepository statementsRepo, IPoliticiansRepository politiciansRepo)
     {
-      _repo = repo;
+      _statementsRepo = statementsRepo;
+      _politiciansRepo = politiciansRepo;
     }
 
     [HttpGet]
     public async Task<ActionResult<List<StatementOutDto>>> GetAllStatements()
     {
-      return Ok(await _repo.GetAllStatements());
+      return Ok(await _statementsRepo.GetAllStatements());
     }
     [HttpGet("{id}")]
     public async Task<ActionResult<StatementOutDto>> GetStatetementById(string id)
     {
-      return Ok(await _repo.GetStatementById(id));
+      return Ok(await _statementsRepo.GetStatementById(id));
     }
     [HttpPost]
     public async Task<ActionResult<StatementOutDto>> AddStatement(StatementDto statementDto)
     {
-      var createdStatement = await _repo.AddStatement(statementDto);
+      if (statementDto.PoliticianId is null)
+      {
+        return ValidationProblem("Nenurodėte politiko kurio pasisakymą bandėte pridėti");
+      }
+      if (statementDto.Link is null || statementDto.Link.Length < 1)
+      {
+        return ValidationProblem("Nenurodėte pasisakymo nuorodos");
+      }
+      if (statementDto.Tags is null || statementDto.Tags.Count < 1)
+      {
+        return ValidationProblem("Reikia pasisakymui parinkti bent vieną žymą");
+      }
+      var existingPolitician = await _politiciansRepo.GetPoliticianById(statementDto.PoliticianId);
+      if (existingPolitician is null)
+      {
+        return ValidationProblem("Nurodytas politikas neegzistuoja");
+      }
+      var createdStatement = await _statementsRepo.AddStatement(statementDto);
       return CreatedAtAction(nameof(GetStatetementById), new { id = createdStatement.StatementId }, createdStatement);
     }
   }

da62131002bbcb089302538a49839b0bf1ba9d67
mxstrong <mantas.ptakauskas@gmail.com>
2021-11-24 19:44:09 +0200

    update editing profiles and fix changePassword bug

---
+++ back/Dtos/Auth/ChangeEmailDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Politics.Dtos
+{
+  public class ChangeEmailDto
+  {
+    public string NewEmail { get; set; }
+  }
+}
